9d585ade938595295a0c7fe145712d2edb73ff19
alikhabazian <khabaziana@gmail.com>
2023-06-08 00:59:43 +0330

    now it is zoomable

--- secret_hitler/lib/game.dart
+++ secret_hitler/lib/game.dart
@@ -44,12 +44,48 @@ class _Game extends State<Game> {
     print(dec);
     List<String> chancellor_list=[...widget.name];
     chancellor_list.remove(chancellor_list[turn]);
-
     if(!first_selected) {
       chancellor = widget.name[(turn+1) % number_players];
     }
     print("chancellor : ${chancellor}");
     print("chancellor list : ${chancellor_list}");
+
+
+    List<TableCell> fac_table = [];
+    for (int i = 0; i < 3; i++) {
+      fac_table.add(new TableCell(
+        verticalAlignment: TableCellVerticalAlignment.top,
+        child: Container(
+          child:Image(image: AssetImage('assets/Search.png')),
+          height: 120,
+          width: 60,
+          color: Colors.red,
+        ),
+      ));
+    }
+    for (int i = 0; i < 3; i++) {
+      fac_table.add(new TableCell(
+        verticalAlignment: TableCellVerticalAlignment.top,
+        child: Container(
+          height: 120,
+          width: 60,
+          color: Colors.red[900],
+        ),
+      ));
+    }
+
+    List<TableCell> lib_table= [];
+    for (int i = 0; i < 6; i++) {
+      lib_table.add(new TableCell(
+        verticalAlignment: TableCellVerticalAlignment.top,
+        child: Container(
+          height: 120,
+          width: 60,
+          color: Colors.blue[400],
+        ),
+      ));
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Roll Assigning'),
@@ -60,6 +96,43 @@ class _Game extends State<Game> {
               children:<Widget>[
 
                 SizedBox(height: 16.0),
+
+                InteractiveViewer(
+                    boundaryMargin: const EdgeInsets.all(1.0),
+                    minScale: 0.1,
+                    maxScale: 2,
+                    child:Table(
+                  border: TableBorder.all(),
+                  columnWidths:const <int, TableColumnWidth>{
+                    0: IntrinsicColumnWidth(),
+                    1: IntrinsicColumnWidth(),
+                    2: IntrinsicColumnWidth(),
+                    3: IntrinsicColumnWidth(),
+                    4: IntrinsicColumnWidth(),
+                    5: IntrinsicColumnWidth(),
+                  },
+                  children:<TableRow>[
+                    TableRow(children: fac_table),
+                    TableRow(children: lib_table),
+                  ]
+                )
+                ),
+                SizedBox(height: 8.0),
+                ClipOval(
+                  child: Material(
+                    color: Colors.red,
+                    child: Container(
+                      child: Text(
+                        '1',
+                        style: TextStyle(fontSize: 20),
+                        textAlign: TextAlign.center,
+                      ),
+                      height: 25.0,
+                      width: 25.0,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
                 Padding(
                     padding: EdgeInsets.all(8.0),
                     child:Text(
