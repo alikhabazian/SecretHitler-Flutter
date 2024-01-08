fd3b960dde04ac6af2b92b2e09c465f7562fabf8
alikhabazian <khabaziana@gmail.com>
2024-01-08 23:52:55 +0330

    web app

--- secret_hitler/build/web/flutter_service_worker.js
+++ secret_hitler/build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "af4152876f48d2e3fd29a34f186ad363",
-"index.html": "4d9daeabf782b5e7ac47e17676440f18",
-"/": "4d9daeabf782b5e7ac47e17676440f18",
-"main.dart.js": "d66fb6a8be024ba905fbe943c91489ac",
+"index.html": "04a599526130be0d9366ab07c48495f9",
+"/": "04a599526130be0d9366ab07c48495f9",
+"main.dart.js": "d17f1b4c78589e6180a4427c7b9b3a63",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
