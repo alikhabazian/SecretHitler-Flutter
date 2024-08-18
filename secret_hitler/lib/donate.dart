ebfcf684cd55332bfa134788dd6ed8ba104552dd
alikhabazian <khabaziana@gmail.com>
2024-08-18 19:27:49 +0330

    edit donate page

--- secret_hitler/lib/donate.dart
+++ secret_hitler/lib/donate.dart
@@ -45,17 +45,17 @@ class _DonatepageState extends State<Donatepage> {
     double height = MediaQuery.of(context).size.height;
     final ButtonStyle style = ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          fetchDataFromNetwork();
-        },
-        tooltip: 'Refresh',
-        child: const Icon(Icons.refresh),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: (){
+      //     fetchDataFromNetwork();
+      //   },
+      //   tooltip: 'Refresh',
+      //   child: const Icon(Icons.refresh),
+      // ),
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text("Donate"),
+        title: const Text("About me"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -67,6 +67,20 @@ class _DonatepageState extends State<Donatepage> {
             //   style: TextStyle(fontSize: 20),
             // ),
             SizedBox(
+                width: width*0.9,
+                // height: 50,
+                child:const Text(
+
+                  "I created this application because I have always enjoyed playing this game and wanted to share that experience with my friends."+
+                  "My passion for gaming and technology inspired me to develop this app, so that others can also enjoy the game with their friends."+
+                  "Your support through donations will help me continue to improve and expand the app, further fueling my passion for creating enjoyable experiences."+
+                  "If you have any questions or feedback, feel free to reach out to me via email at khabaziana@gmail.com.",
+                  textAlign: TextAlign.justify,
+
+                )
+            ),
+
+            SizedBox(
               width: width*0.9,
               height: 50,
               child:ElevatedButton(
@@ -75,52 +89,52 @@ class _DonatepageState extends State<Donatepage> {
 
 
 
-                  final Uri _url = Uri.parse('https://hamibash.com/secrethitler/');
-                  await launchUrl(_url);
+                  final Uri url = Uri.parse('https://portfolio-eight-iota-38.vercel.app/');
+                  await launchUrl(url);
 
 
                   // _launchURL('https://youtu.be/mbGXIDYdtas?si=l8QQgu_Bm3-i99jI');
 
                 },
-                child: const Text('Donation Link Hamibash'),
+                child: const Text('Portfolio link'),
               ),
             ),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Center(
-                      child: Text(
-                        title,
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    ),
-                    Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Container(
-                          width: width * 0.9,
-                          height: 30,
-                          child: LinearProgressIndicator(
-                            value: progressValue,
-                            minHeight: 40,
-                            semanticsLabel: 'Linear progress indicator',
-                            semanticsValue:
-                            'What percentage of the donation has been completed?',
-                          ),
-                        ),
-                        const Text(
-                          'What percentage of the donation has been completed?', // Replace X with the actual percentage
-                          style: TextStyle(fontSize: 14, color: Colors.black54),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-
-            ),
+            // Card(
+            //   child: Padding(
+            //     padding: const EdgeInsets.all(8.0),
+            //     child: Column(
+            //       children: [
+            //         Center(
+            //           child: Text(
+            //             title,
+            //             style: TextStyle(fontSize: 30),
+            //           ),
+            //         ),
+            //         Stack(
+            //           alignment: Alignment.center,
+            //           children: [
+            //             Container(
+            //               width: width * 0.9,
+            //               height: 30,
+            //               child: LinearProgressIndicator(
+            //                 value: progressValue,
+            //                 minHeight: 40,
+            //                 semanticsLabel: 'Linear progress indicator',
+            //                 semanticsValue:
+            //                 'What percentage of the donation has been completed?',
+            //               ),
+            //             ),
+            //             const Text(
+            //               'What percentage of the donation has been completed?', // Replace X with the actual percentage
+            //               style: TextStyle(fontSize: 14, color: Colors.black54),
+            //             ),
+            //           ],
+            //         ),
+            //       ],
+            //     ),
+            //   ),
+            //
+            // ),
           ],
         ),
       ),
